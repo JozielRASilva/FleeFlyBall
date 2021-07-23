e0b648ea3f9d62c244d1dd605b555174a4911a08
JozielRASilva <46303501+JozielRASilva@users.noreply.github.com>
2021-07-23 10:43:21 -0300

    Config team controller

--- Assets/Scripts/Team/TeamController.cs
+++ Assets/Scripts/Team/TeamController.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 public class TeamController : Singleton<TeamController>
 {
 
     private List<TeamGroup> teamGroup = new List<TeamGroup>();
 
+    protected override void Awake()
+    {
+        base.Awake();
+
+        teamGroup = GetComponentsInChildren<TeamGroup>().ToList();
+    }
+
+    public TeamGroup GetPlayerGroup()
+    {
+        return teamGroup.Find(t => t.isPlayerGroup);
+    }
+
+    public TeamGroup GetOpponentGroup()
+    {
+        return teamGroup.Find(t => !t.isPlayerGroup);
+    }
 
 
 }
