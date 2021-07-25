7a15ecd76064b89b3ff8e90132effdc1f79911c9
JozielRASilva <46303501+JozielRASilva@users.noreply.github.com>
2021-07-25 20:36:05 -0300

    AI input setup

---
+++ Assets/Scripts/AIs/AiInputs/AIInputAxis.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AIInputAxis : AIInputBase<Vector2>
+{
+    public override Vector2 GetValue(AICharacterBase AI)
+    {
+        Vector2 value = Vector2.zero;
+
+        return value;
+    }
+
+
+
+}
