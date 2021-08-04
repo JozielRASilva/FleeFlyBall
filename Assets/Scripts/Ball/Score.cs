4dd1260ddd220392e47c137e2c5cbc214f52a854
Guigsr <85503692+Guigsr@users.noreply.github.com>
2021-08-04 18:30:17 -0300

    score

--- Assets/Scripts/Ball/Score.cs
+++ Assets/Scripts/Ball/Score.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     public int redScore;
     public int greenScore;
+
+    public TextMeshProUGUI redTextScore;
+    public TextMeshProUGUI greenTextScore;
+
+    private void Update()
+    {
+        redTextScore.text = redScore.ToString();
+        greenTextScore.text = greenScore.ToString();
+    }
 }
